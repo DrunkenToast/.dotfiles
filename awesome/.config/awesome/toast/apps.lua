53653aee677511cc8bdd89d31d5159109d1a1ea2
Peter Leconte <peter5leconte@gmail.com>
2022-11-09 16:30:09 +0100

    delete awesome

--- awesome/.config/awesome/toast/apps.lua
+++
@@ -1,7 +0,0 @@
-apps = {
-    terminal = "alacritty",
-    editor = os.getenv("EDITOR") or "nvim",
-    rofi_run = "rofi -show drun",
-    rofi_power = "bash $HOME/.config/rofi/scripts/power",
-    lock = "i3lock -n -i ~/Pictures/Wallpapers/FXdgQSz.png",
-}
