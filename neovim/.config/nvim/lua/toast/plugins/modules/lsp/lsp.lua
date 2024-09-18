225502ff97895114f20ad7a71eb11188757fec1f
Peter Leconte <peter5leconte@gmail.com>
2024-09-18 14:42:37 +0200

    chore: apply updates & breaking changes

--- neovim/.config/nvim/lua/toast/plugins/modules/lsp/lsp.lua
+++ neovim/.config/nvim/lua/toast/plugins/modules/lsp/lsp.lua
@@ -58,7 +58,7 @@ return {
             require('mason-lspconfig').setup({
                 ensure_installed =
                 {
-                    'tsserver',
+                    'ts_ls',
                     'rust_analyzer',
                     'lua_ls',
                     'marksman',
@@ -107,14 +107,14 @@ return {
                     volar = function()
                         require('lspconfig').volar.setup({})
                     end,
-                    tsserver = function()
+                    ts_ls = function()
                         local vue_typescript_plugin = require('mason-registry')
                             .get_package('vue-language-server')
                             :get_install_path()
                             .. '/node_modules/@vue/language-server'
                             .. '/node_modules/@vue/typescript-plugin'
 
-                        require('lspconfig').tsserver.setup({
+                        require('lspconfig').ts_ls.setup({
                             init_options = {
                                 plugins = {
                                     {
