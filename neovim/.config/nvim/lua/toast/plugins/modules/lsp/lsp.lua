78988a29074631b40eac0a6125061fb153e50491
Peter Leconte <peter5leconte@gmail.com>
2025-12-12 14:30:39 +0100

    hopefully going to get a successful build got damn it

--- neovim/.config/nvim/lua/toast/plugins/modules/lsp/lsp.lua
+++ neovim/.config/nvim/lua/toast/plugins/modules/lsp/lsp.lua
@@ -1,11 +1,9 @@
 return {
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        event = { "BufReadPre", "BufNewFile" },
+        'neovim/nvim-lspconfig',
+        lazy = false,
         dependencies = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },
             {
                 'williamboman/mason.nvim',
                 build = function()
@@ -21,7 +19,7 @@ return {
             { 'hrsh7th/cmp-path' },
             { 'hrsh7th/cmp-nvim-lua' },
 
-            { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+            -- { 'hrsh7th/cmp-nvim-lsp-signature-help' },
 
             { 'saadparwaiz1/cmp_luasnip' },
 
@@ -54,12 +52,68 @@ return {
                     },
                 },
                 underline = true,
+                update_in_insert = true,
+                severity_sort = true,
+                virtual_lines = false,
             }
 
-            local lsp_zero = require('lsp-zero')
-            local lsp_kind = require('lspkind')
+
+            local lspkind = require('lspkind')
             local cmp = require('cmp')
-            lsp_zero.preset('recommended')
+
+            --- cmp {{{
+            -- vim.lsp.config('*', {
+            --     capabilities = lsp_capabilities,
+            -- }
+            -- )
+
+            -- local lspconfig_defaults = util.default_config
+            -- lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+            --     'force',
+            --     lspconfig_defaults.capabilities,
+            --     require('cmp_nvim_lsp').default_capabilities()
+            -- )
+
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                }),
+
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = "symbol",
+                        menu = "",
+                    }),
+                },
+
+                sources = cmp.config.sources({
+                    { name = "luasnip" },
+                    { name = "nvim_lsp" },
+                    { name = "nvim_lua" },
+                    -- { name = "nvim_lsp_signature_help" },
+                    { name = "buffer" },
+                    { name = "path" },
+                    { name = "crates" },
+                }),
+
+                window = {
+                    -- completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+            })
+
+            --- }}}
 
             --- Mason {{{
             require('mason').setup({}) -- TODO check this
@@ -73,86 +127,60 @@ return {
                     'vue_ls',
                     'vtsls',
                 },
-                handlers = {
-                    lsp_zero.default_setup,
-                    rust_analyzer = function()
-                        local rt = require('rust-tools')
-                        rt.setup({
-                            server = {
-                                on_attach = function(_, bufnr)
-                                    vim.keymap.set('n', 'K', rt.hover_actions.hover_actions,
-                                        { buffer = bufnr })
-                                end,
-                                settings = {
-                                    ["rust-analyzer"] = {
-                                        cargo = {
-                                            allFeatures = true,
-                                        },
-                                        procMacro = {
-                                            ignored = {
-                                                ['napi-derive'] = { 'napi' },
-                                            },
-                                        }
-                                    },
-                                },
-                            },
-                        })
-                    end,
-                    omnisharp = function()
-                        -- vim.env.DOTNET_ROOT = '/usr/local/share/dotnet'
-                        -- vim.env.PATH = vim.env.PATH .. ':' .. vim.env.DOTNET_ROOT
-                        local pid = vim.fn.getpid()
-
-                        require('lspconfig').omnisharp.setup({
-                            cmd_env = {
-                                -- DOTNET_ROOT = '/usr/local/share/dotnet',
-                                -- PATH = '/usr/local/share/dotnet' .. ':' .. vim.env.PATH,
-                            },
-                            cmd = { "omnisharp", '--languageserver', '--hostPID', tostring(pid) },
-                            handlers = {
-                                ["textDocument/definition"] = require('omnisharp_extended').handler,
-                            },
-                        })
-                    end,
-                    vue_ls = function()
-                    end,
-                    -- vtsls is used in favor for vue support
-                    -- ts_ls = function()
-                    --     -- local vue_typescript_plugin = require('mason-registry')
-                    --     --     .get_package('vue-language-server')
-                    --     --     :get_install_path()
-                    --     --     .. '/node_modules/@vue/language-server'
-                    --     --     .. '/node_modules/@vue/typescript-plugin'
-                    --
-                    --     require('lspconfig').ts_ls.setup({
-                    --         init_options = {
-                    --             -- plugins = {
-                    --             --     {
-                    --             --         name = "@vue/typescript-plugin",
-                    --             --         location = vue_typescript_plugin,
-                    --             --         languages = { 'javascript', 'typescript', 'vue' }
-                    --             --     },
-                    --             -- }
-                    --         },
-                    --         filetypes = {
-                    --             'javascript',
-                    --             'javascriptreact',
-                    --             'javascript.jsx',
-                    --             'typescript',
-                    --             'typescriptreact',
-                    --             'typescript.tsx',
-                    --             -- 'vue',
-                    --         },
-                    --     })
-                    -- end,
-                },
+                -- handlers = {
+                --     rust_analyzer = function()
+                --         local rt = require('rust-tools')
+                --         rt.setup({
+                --             server = {
+                --                 on_attach = function(_, bufnr)
+                --                     vim.keymap.set('n', 'K', rt.hover_actions.hover_actions,
+                --                         { buffer = bufnr })
+                --                 end,
+                --                 settings = {
+                --                     ["rust-analyzer"] = {
+                --                         cargo = {
+                --                             allFeatures = true,
+                --                         },
+                --                         procMacro = {
+                --                             ignored = {
+                --                                 ['napi-derive'] = { 'napi' },
+                --                             },
+                --                         }
+                --                     },
+                --                 },
+                --             },
+                --         })
+                --     end,
+                --     omnisharp = function()
+                --         -- vim.env.DOTNET_ROOT = '/usr/local/share/dotnet'
+                --         -- vim.env.PATH = vim.env.PATH .. ':' .. vim.env.DOTNET_ROOT
+                --         local pid = vim.fn.getpid()
+                --
+                --         require('lspconfig').omnisharp.setup({
+                --             cmd_env = {
+                --                 -- DOTNET_ROOT = '/usr/local/share/dotnet',
+                --                 -- PATH = '/usr/local/share/dotnet' .. ':' .. vim.env.PATH,
+                --             },
+                --             cmd = { "omnisharp", '--languageserver', '--hostPID', tostring(pid) },
+                --             handlers = {
+                --                 ["textDocument/definition"] = require('omnisharp_extended').handler,
+                --             },
+                --         })
+                --     end,
+                --     -- vue_ls = function()
+                --     -- end,
+                -- },
             })
             --}}}
-            --
 
-            -- Vue {{{
+            vim.lsp.enable('tailwindcss')
+
+            -- Vue & TypeScript {{{
             local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
                 '/vue-language-server' .. '/node_modules/@vue/language-server'
+
+            local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+
             local vue_plugin = {
                 name = '@vue/typescript-plugin',
                 location = vue_language_server_path,
@@ -170,48 +198,40 @@ return {
                         },
                     },
                 },
-                filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+                filetypes = tsserver_filetypes,
+                on_attach = function()
+                    vim.lsp.enable({ 'vue_ls' })
+                end
             }
 
-            local vue_ls_config = {
-                on_init = function(client)
-                    client.handlers['tsserver/request'] = function(_, result, context)
-                        local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
-                        if #clients == 0 then
-                            vim.notify('Could not found `vtsls` lsp client, vue_lsp would not work without it.',
-                                vim.log.levels.ERROR)
-                            return
-                        end
-                        local ts_client = clients[1]
+            local vue_ls_config = {}
 
-                        local param = unpack(result)
-                        local id, command, payload = unpack(param)
-                        ts_client:exec_cmd({
-                            title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-                            command = 'typescript.tsserverRequest',
-                            arguments = {
-                                command,
-                                payload,
-                            },
-                        }, { bufnr = context.bufnr }, function(_, r)
-                            local response_data = { { id, r.body } }
-                            ---@diagnostic disable-next-line: param-type-mismatch
-                            client:notify('tsserver/response', response_data)
-                        end)
-                    end
-                end,
-            }
-
-            -- nvim 0.11 or above
             vim.lsp.config('vtsls', vtsls_config)
             vim.lsp.config('vue_ls', vue_ls_config)
-            vim.lsp.enable({ 'vtsls', 'vue_ls' })
+            vim.lsp.enable({ 'vtsls' })
+
+            -- }}}
+
+            -- Eslint {{{
+            local base_on_attach = vim.lsp.config.eslint.on_attach
+            vim.lsp.config("eslint", {
+                on_attach = function(client, bufnr)
+                    if not base_on_attach then return end
+
+                    base_on_attach(client, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        command = "LspEslintFixAll",
+                    })
+                end,
+            })
+
+            vim.lsp.enable('eslint')
             -- }}}
 
 
             -- Swift {{{
-            local lspconfig = require('lspconfig')
-            lspconfig.sourcekit.setup {
+            vim.lsp.config('sourcekit', {
                 capabilities = {
                     workspace = {
                         didChangeWatchedFiles = {
@@ -219,61 +239,41 @@ return {
                         },
                     },
                 },
-            }
+            })
+            vim.lsp.enable('sourcekit')
             -- }}}
 
             -- Godot {{{
             vim.fn.setenv('GDScript_Port', 6005)
-            lsp_zero.configure('gdscript', { force_setup = true })
+            vim.lsp.enable('gdscript')
             --- }}}
 
-            local cmp_config = lsp_zero.defaults.cmp_config({
-                mapping = {
-                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-                },
-                formatting = {
-                    format = lsp_kind.cmp_format({
-                        mode = 'symbol',
-                        menu = "",
-                    })
-                },
-                sources = {
-                    { name = 'luasnip' },
-                    { name = 'nvim_lsp' },
-                    { name = 'nvim_lua' }, -- nvim-cmp source for neovim Lua API
-                    { name = "nvim_lsp_signature_help" },
-                    { name = "buffer" },
-                    { name = "path" },
-                    { name = "crates" }, -- For rust crates
-                }
-            })
 
-            cmp.setup(cmp_config)
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'LSP actions',
+                callback = function(event)
+                    local opts = { remap = false, silent = true, buffer = event.buf }
 
-            vim.diagnostic.config({
-                update_in_insert = true,
-                underline = true,
-                severity_sort = true,
-                virtual_lines = false,
-            })
-
-            lsp_zero.on_attach(
-                function(client, bufnr)
-                    lsp_zero.default_keymaps({ buffer = bufnr })
-                    local opts = { remap = false, silent = true, buffer = bufnr }
+                    -- vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                    -- vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                    -- vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                    -- vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                    -- vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
                     vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-                    -- vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
                     vim.keymap.set('n', '<leader>bf', function() vim.lsp.buf.format { async = true } end, opts)
-                    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 
+                    -- With plugins
                     vim.keymap.set('n', 'gr', '<cmd>Trouble lsp_references<cr>', { buffer = bufnr })
                     vim.keymap.set('n', '<leader>r', '<cmd>Lspsaga rename<cr>', opts)
                     vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<cr>', opts)
-                end
-            )
-
-            lsp_zero.setup()
+                end,
+            })
 
             require("luasnip.loaders.from_vscode").lazy_load({
                 include = nil, -- Load all languages
